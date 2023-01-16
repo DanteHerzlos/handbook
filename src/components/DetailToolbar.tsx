1826c3931b0e1b43e3327b5edb8ca682439a55f6
DanteHerzlos <streat13@gmail.com>
2023-01-16 10:20:51 +0300

    add toolbar for details page

---
+++ src/components/DetailToolbar.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import cl from "../styles/components/DetailToolbar.module.css"
+import Button from "../components/UI/Button"
+import DeleteCardForm from "../components/forms/DeleteCardForm"
+import EditCardForm from "../components/forms/EditCardForm"
+
+const DetailToolbar = () => {
+  const navigate = useNavigate()
+
+  return (
+    <div className={cl.toolbar}>
+      <div>
+        <Button onClick={() => navigate(-1)}>Back</Button>
+      </div>
+      <div className={cl.toolbar__right}>
+        <Button>Импорт в PDF</Button>
+        <EditCardForm />
+        <DeleteCardForm />
+      </div>
+    </div>
+  )
+}
+
+export default DetailToolbar

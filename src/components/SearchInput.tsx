8cf0cb653bb49148e742b7e9d23fcea796961770
DanteHerzlos <streat13@gmail.com>
2023-01-16 10:21:19 +0300

    modify serach input adding droplist

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import cl from '../styles/components/SearchInput.module.css';
+import React from "react"
+import cl from "../styles/components/SearchInput.module.css"
 
 const SearchInput = () => {
   return (
     <div>
       <input className={cl.input} placeholder="Поиск..." />
+      <select
+        className={cl.select}
+        name="filters"
+        id="filters"
+        defaultValue={"owners"}
+      >
+        <option value="owners">Владелец</option>
+        <option value="land_plot_index">Кад. н. участка</option>
+        <option value="house_index">Кад. н. дома</option>
+        <option value="address">Адрес</option>
+        <option value="postal_address">Кор. адрес</option>
+        <option value="email">Email</option>
+      </select>
     </div>
-  );
-};
+  )
+}
 
-export default SearchInput;
+export default SearchInput

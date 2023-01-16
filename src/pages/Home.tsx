cff5f3a76a069f3b3778748b11cfb53ad2ca9702
DanteHerzlos <streat13@gmail.com>
2023-01-16 10:22:09 +0300

    add form to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,9 @@ import React, { useState, useEffect } from "react"
 import cl from "../styles/pages/Home.module.css"
 import Owner from "../components/Owner"
 import SearchInput from "../components/SearchInput"
-
 import { Link } from "react-router-dom"
 import { ICard } from "../types/ICard"
-
-
+import AddCardForm from "../components/forms/AddCardForm"
 
 const Home = () => {
   const [owners, setOwners] = useState<ICard[]>([])
@@ -16,13 +14,13 @@ const Home = () => {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const cards = await (window as any).electronAPI.getAllCards()
       setOwners(cards)
-      console.log(cards)
     }
     fetchCards()
   }, [])
 
   return (
     <div className={cl.container}>
+      <AddCardForm/>
       <SearchInput />
       <hr />
       {owners.map((el) => (
